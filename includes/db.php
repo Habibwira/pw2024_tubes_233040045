24eadeb3af8161a120da099913251d42dc02848d
Habibwira <fhabibwira@gmail.com>
2024-06-08 14:30:54 +0700

    update file

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 // Define database connection variables
-$servername = "cineversequuy.online";
+$servername = "localhost";
 $username = "u751466727_hwf"; // Sesuaikan dengan username MySQL Anda
 $password = "Habibwf9"; // Sesuaikan dengan password MySQL Anda
 $dbname = "u751466727_cineverse"; // Ganti dengan nama database Anda
